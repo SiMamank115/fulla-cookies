964a62a556dd7740991cf01aaed146f1698f2949
SiMamank115 <faizfullafalisa@gmail.com>
2022-02-12 05:51:28 +0700

    minggu-1 sekolah | assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,21 @@ const classBreaker = (e = "") => e.split(" ")
 document.onreadystatechange = () => {
     if (document.readyState == "complete") {
         document.body.classList.remove("d-none")
-        document.body.classList.add("d-flex")
-        document.querySelector(".navbar-brand").classList.add("navbar-brand-show");
-        if(typeof pillNum != "undefined"){
-            setTimeout(() => {
-                document.querySelector(".nav-top").children[pillNum - 1].classList.add(...classBreaker("uc-active disabled"))
-            }, 400);
-            document.querySelector(".nav-sidebar").children[pillNum - 1].classList.add(...classBreaker("uc-active disabled"))
-            document.querySelector(".nav-bottom").children[pillNum - 1].classList.add(...classBreaker("fw-bold nav-link-custom"))
-            document.querySelector(".nav-bottom").children[pillNum - 1].setAttribute("style","pointer-events: none;cursor: default;")
-        }
+        setTimeout(() => {
+            if(typeof ready == "function") {
+                ready()
+            }
+            document.body.classList.add("d-flex")
+            document.querySelector(".navbar-brand").classList.add("navbar-brand-show");
+            if(typeof pillNum != "undefined"){
+                setTimeout(() => {
+                    document.querySelector(".nav-top").children[pillNum - 1].classList.add(...classBreaker("uc-active disabled"))
+                }, 400);
+                document.querySelector(".nav-sidebar").children[pillNum - 1].classList.add(...classBreaker("uc-active disabled"))
+                document.querySelector(".nav-bottom").children[pillNum - 1].classList.add(...classBreaker("fw-bold nav-link-custom"))
+                document.querySelector(".nav-bottom").children[pillNum - 1].setAttribute("style","pointer-events: none;cursor: default;")
+            }
+        }, 100);
     }
 }
 document.onscroll = () => {
