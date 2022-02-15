2c98e264fd45519cc01d2b3a19a2d8736b78c93a
SiMamank115 <faizfullafalisa@gmail.com>
2022-02-15 09:40:24 +0700

    membuat kontak & logic nya

--- application/controllers/Kontak.php
+++ application/controllers/Kontak.php
@@ -1,11 +1,43 @@
 <?php
 class Kontak extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library("session");
+    }
     public function index()
     {
         $data["pill"] = 4;
+        $this->load->helper("form");
         $this->load->view("templates/header");
-        $this->load->view("kontak/index",$data);
+        $this->load->view("kontak/index", $data);
         $this->load->view("templates/footer");
+    }
+    public function send()
+    {
+        if ($this->input->post("submit")) {
+            $ip = curl_init();
+            curl_setopt_array($ip, array(CURLOPT_URL => "https://ipinfo.io?token=6f5c214ada048e", CURLOPT_RETURNTRANSFER => true));
+            $user_ip = json_decode(curl_exec($ip), TRUE)["ip"];
+            curl_close($ip);
+            $nama = $this->input->post("nama");
+            $email = $this->input->post("email");
+            $nomor = $this->input->post("nomor");
+            $pesan = $this->input->post("pesan");
+            $text = "<b>Fulla Cookies contact</b>\n\nip : <b>$user_ip</b>\nnama : <b>$nama</b>\nemail : <b>$email</b>\nnomer : <b>$nomor</b>\npesan : \n\n<b>$pesan</b>";
+            $url = "https://api.telegram.org/bot5242312802%3AAAGeWwofFXLFf2b64v_FzqQNkx2TfxVRsDs/sendMessage?parse_mode=HTML&chat_id=@fullacontact";
+            $url = $url . "&text=" . urlencode($text);
+            $send = curl_init();
+            curl_setopt_array($send, [CURLOPT_URL => $url,CURLOPT_RETURNTRANSFER => true]);
+            $result = json_decode(curl_exec($send),TRUE);
+            curl_close($send);
+            if($result["ok"]) {
+                $this->session->set_flashdata("terkirim",true);
+            } else {
+                $this->session->set_flashdata("terkirim",false);
+            }
+            redirect(base_url("kontak"));
+        }
     }
 }
--- application/views/kontak/index.php
+++ application/views/kontak/index.php
@@ -1,1 +1,52 @@
-<p>kontak/index</p>
+<?php
+$e = $this->session->flashdata("terkirim");
+if(isset($e) && $e) {
+    echo "<script>alert('terkirim')</script>";
+} else if(isset($e)) {
+    echo "<script>alert('kasian')</script>";
+}
+?>
+<div class="container-md row mx-auto bg-light d-flex g-0 flex-wrap mt-4">
+    <div class="col-lg-10 p-4">
+        <?=form_open("kontak/send")?>
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control custom-control" id="form_nama" placeholder="John Doe" name="nama">
+            <label for="form_nama">Nama</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="email" required class="form-control custom-control" id="form_email" placeholder="john@gmail.com" name="email">
+            <label for="form_email">Email</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="number" required class="form-control custom-control" id="form_nomor" placeholder="087734529XX" name="nomor">
+            <label for="form_nomer">Nomor telp.</label>
+        </div>
+        <div class="form-floating mb-3">
+            <textarea style="height: 100px;" type="number" required class="form-control custom-control" id="form_nomer" placeholder="" name="pesan"></textarea>
+            <label for="form_nomer">pesan</label>
+        </div>
+        <input class="btn btn-custom float-end" type="submit" name="submit" value="Kirim !">
+        </form>
+    </div>
+    <div class="col-lg-2"></div>
+</div>
+<style>
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: #666;
+        border-radius: 20px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: #ddd;
+        border-radius: 20px;
+    }
+
+    .custom-control:focus {
+        border-color: #ddb892 !important;
+        box-shadow: 0 0 0 0.25rem #edc53140 !important;
+    }
+</style>
